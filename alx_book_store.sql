1196b0f866a50e702a60b1817a77bac86634f7ca
Ndata <wisdomndata@gmail.com>
2025-06-28 22:48:53 +0100

    MySQL Database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -29,7 +29,7 @@ CREATE TABLE Orders (
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
-CREATE TABLE 0rder_Details (
+CREATE TABLE Order_Details (
     orderdetailid INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
