ebef6a54e61ceee4ed9b1f94a24e84169dbd70b5
Ndata <wisdomndata@gmail.com>
2025-06-29 00:00:40 +0100

    MySQL Database

---
+++ MySQLServer.py
@@ -0,0 +1,18 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = "localhost",
+    user = 'root',
+    password = "Wisdomndata.",
+    # database = "wisdom"
+)
+
+if mydb.is_connected():
+    cursor = mydb.cursor()
+    print("Database 'alx_book_store' created successfully!")
+
+else:
+    print('Error while connecting to data basae')
+
+cursor.close()
+mydb.close()
