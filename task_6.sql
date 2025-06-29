29555c5bf8dc477bbeb4a65a1fd813783369edf5
Ndata <wisdomndata@gmail.com>
2025-06-29 21:58:09 +0100

    SQL

--- task_6.sql
+++ task_6.sql
@@ -6,13 +6,13 @@ INSERT INTO customer (
 )
 
 VALUES (
-    2, 'Blessing Malik', 'bmalik@sandtech.com', "124 Happiness Ave."
+    2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'
 ),
 (
-    3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'
+    3, 'Obed Ehoneah', 'eobed@sandtech.com', '124 Happiness  Ave.'
 ),
 (
-    4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.'
+    4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '124 Happiness  Ave.'
 );
 
 SELECT * FROM customer;
