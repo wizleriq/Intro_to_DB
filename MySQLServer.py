24074c1a4d72279eab2dc78288a157effc5539c0
Ndata <wisdomndata@gmail.com>
2025-06-29 03:12:51 +0100

    MySQL Database

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,20 +1,30 @@
 import mysql.connector
 
-
-mydb = mysql.connector.connect(
+try:
+    mydb = mysql.connector.connect(
     host = "localhost",
     user = 'root',
     password = "Wisdomndata.",
     # database = "wisdom"
 )
+    if mydb.is_connected():
+        cursor = mydb.cursor()
+        cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+        print("Database 'alx_book_store' created successfully!")
 
-if mydb.is_connected():
-    cursor = mydb.cursor()
-    cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
-    print("Database 'alx_book_store' created successfully!")
-
-else:
-    print('Error while connecting to data basae')
-
+except mysql.connector.Error as err:
+    print('Error while connecting to data base')
+    print("MySQL Error:", err)
+    
 cursor.close()
 mydb.close()
+# else:
+#     print('Error while connecting to data basae')
+
+
+
+
+
+
+
+
